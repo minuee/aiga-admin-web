e4b38852819f86f9c21200fdd716d9d45324d0f9
nohsn <minuee@kormedi.com>
2025-05-26 13:42:41 +0900

    prefix_update3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { redirect } from 'next/navigation';
 export default function Home({}) {
-  const adminPrefix = process.env.NEXT_PUBLIC_PREFIX;
-  redirect(`${adminPrefix}/v1/dashboard`);
+  redirect('/v1/dashboard');
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,24 +6,24 @@ export async function middleware(request: NextRequest) {
     const { nextUrl, cookies } = request;
     const { origin, pathname } = nextUrl;
     const accessToken = cookies.get(process.env.NEXT_PUBLIC_AUTH_TOKEN);
-    const adminPrefix = process.env.NEXT_PUBLIC_PREFIX;
     console.log('accessToken',process.env.NEXT_PUBLIC_AUTH_TOKEN,accessToken)
     if (accessToken !== undefined) {
-        if ( pathname.includes("/auth/sign-in")) {
-            return NextResponse.redirect(new URL(`${adminPrefix}/v1/dashboard`,request.url))
+        if ( pathname === "/auth/sign-in") {
+            return NextResponse.redirect(new URL(`/v1/dashboard`,request.url)
+            )
         }else{
             return NextResponse.next();
         }
         
     }else{
         console.log('pathname',pathname)
-        if ( pathname.includes("/auth/sign-in")) {
+        if ( pathname === "/auth/sign-in") {
             return NextResponse.next();
             
         }else{
             console.log('pathname else',pathname)
             // 로그인이 필요 없는 페이지는 그냥 다음 요청으로 진행
-            return NextResponse.rewrite(new URL(`${adminPrefix}/auth/sign-in`, request.url));
+            return NextResponse.rewrite(new URL("/auth/sign-in", request.url));
         }
     }
 }
